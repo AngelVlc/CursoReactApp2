006c69cb7134090704f6bfb606450f4bf9515a8d
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-10 19:15:08 +0100

    Ordenar lista.

--- src/app.js
+++ src/app.js
@@ -18,12 +18,12 @@ const first = store.dispatch(addExpense({ description: 'nuevo', createdAt: 1000,
 const second = store.dispatch(addExpense({ description: 'nuevo2' }));
 store.dispatch(addExpense({ description: 'nuevo3', createdAt: 6000, amount: 90 }));
 store.dispatch(addExpense({ description: 'nuevo4', createdAt: 7000, amount: 75 }));
-store.dispatch(setTextFilter('nuevo3'));
+//store.dispatch(setTextFilter('nuevo3'));
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
