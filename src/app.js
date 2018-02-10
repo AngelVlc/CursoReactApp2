b189d8586406d5292737ab1b5f2d7cb71c83db96
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-10 09:46:08 +0100

    Redux dispatch action.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0}) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+
+        case 'DECREMENT':
+            return { count: state.count - 1 };
+
+        case 'RESET':
+            return { count: 0 };
+
+        default:
+            return state;
+    }
 });
 
 store.getState();
+
+console.log('state', store.getState());
+
+
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+
+
+store.dispatch({ type: 'DECREMENT' });
+
+store.dispatch({ type: 'RESET' });
 
 console.log('state', store.getState());
