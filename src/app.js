20f3800a733bafd0193271d2761eff7d680aaef8
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-10 09:55:19 +0100

    Redux acciones con parametros.

--- src/app.js
+++ src/app.js
@@ -3,32 +3,52 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return { count: state.count + 1 };
+            const increment = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return { count: state.count + increment };
 
         case 'DECREMENT':
-            return { count: state.count - 1 };
+            const decrement = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+            return { count: state.count - decrement };
 
         case 'RESET':
             return { count: 0 };
+
+        case 'SET':
+            return { count: action.set }
 
         default:
             return state;
     }
 });
 
-store.getState();
 
-console.log('state', store.getState());
+//para enterarse cuando cambia el store
+const unsubscribe = store.subscribe(() => {
+    console.log('state', store.getState());
+
+});
 
 
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
 
 store.dispatch({ type: 'INCREMENT' });
+
+// para parar la subscripcion
+//unsubscribe();
+
 store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
 
-
-store.dispatch({ type: 'DECREMENT' });
-
+});
 store.dispatch({ type: 'RESET' });
 
-console.log('state', store.getState());
+
+store.dispatch({
+    type: 'SET',
+    set: 100
+});
