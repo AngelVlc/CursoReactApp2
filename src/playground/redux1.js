8b84755321dae82f2a25e6c607c9cdf4ef6107e1
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-10 10:50:30 +0100

    Reduz reducer.

--- src/playground/redux1.js
+++ src/playground/redux1.js
@@ -3,49 +3,52 @@ import { createStore } from 'redux';
 // Action generators - functions that return action objects
 
 const incrementCount = ({ incrementBy = 1 } = {}) => ({
-  type: 'INCREMENT',
-  incrementBy
+    type: 'INCREMENT',
+    incrementBy
 });
 
 const decrementCount = ({ decrementBy = 1 } = {}) => ({
-  type: 'DECREMENT',
-  decrementBy
+    type: 'DECREMENT',
+    decrementBy
 });
 
 const setCount = ({ count }) => ({
-  type: 'SET',
-  count
+    type: 'SET',
+    count
 });
 
 const resetCount = () => ({
-  type: 'RESET'
+    type: 'RESET'
 });
 
-const store = createStore((state = { count: 0 }, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + action.incrementBy
-      };
-    case 'DECREMENT':
-      return {
-        count: state.count - action.decrementBy
-      };
-    case 'SET':
-      return {
-        count: action.count
-      };
-    case 'RESET':
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
-});
+// Reducer = pure function
+const countReducer = (state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - action.decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
+    console.log(store.getState());
 });
 
 store.dispatch(incrementCount({ incrementBy: 5 }))
