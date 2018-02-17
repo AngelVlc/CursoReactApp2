39d5841486ecfdd324f3ca0b5a10fa42ab106a18
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-17 19:40:29 +0100

    Numeral js.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpanseListItem = ({id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}>
          <h3>{description}</h3>
         </Link>
-        <p><strong>Amount: </strong>{amount}</p>
-        <p><strong>Created At: </strong>{createdAt}</p>
+        <p><strong>Amount: </strong>{numeral(amount / 100).format('$0,0.00')}</p>
+        <p><strong>Created At: </strong>{moment(createdAt).format('DD/MM/YYYY')}</p>
       
     </div>
 );
