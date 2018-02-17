c320bcee14b88c0ab52fc976e2f62bad089b7a00
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-17 18:57:37 +0100

    Cambios para que los css estén fuera del bundle.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
         entry: './src/app.js',
@@ -17,17 +20,30 @@ module.exports = (env) => {
             },
             {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ],
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
-        
+        plugins: [
+            CSSExtract
+        ],
         //con el source-map se crea un fichero externo con el .map que solo 
         //se usará cuando se abran los devtools en el navegador
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
