273f1c332839605aa0cd114b358051da1fd8dbca
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-18 18:21:13 +0100

    Cambio los test para que funcionen con las llamadas asíncronas a firebase.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,13 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
-    const action = removeExpense({ id: 'elId'});
+    const action = removeExpense({ id: 'elId' });
 
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
@@ -9,38 +15,82 @@ test('should setup remove expense action object', () => {
     });
 });
 
-test('should setup add expense action object with provided object', () => {
+test('should setup add expense action object with provided values', () => {
+    const action = addExpense(expenses[2]);
+
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: expenses[2]
+    });
+});
+
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({});
+
     const expenseData = {
-        description: 'desc',
-        amount: 15678,
-        createdAt: 1000,
-        note: 'note'
-    };
+        description: 'Mouse',
+        amount: 3000,
+        note: 'note',
+        createdAt: 5000
+    }
+    store.dispatch(startAddExpense(expenseData))
+        .then(() => {
+            const actions = store.getActions();
+            expect(actions[0]).toEqual({
+                type: 'ADD_EXPENSE',
+                expense: {
+                    id: expect.any(String),
+                    ...expenseData
+                }
+            });
 
-    const action = addExpense(expenseData);
-
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
-    });
+            return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        }).then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseData);
+            done();
+        });
 });
 
-test('should setup add expense action object with default object', () => {
-    const expenseData = { };
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({});
 
-    const action = addExpense(expenseData);
+    const expenseDefault = {
+        description: '',
+        amount: 0,
+        note: '',
+        createdAt: 0
+    }
+    store.dispatch(startAddExpense(expenseDefault))
+        .then(() => {
+            const actions = store.getActions();
+            expect(actions[0]).toEqual({
+                type: 'ADD_EXPENSE',
+                expense: {
+                    id: expect.any(String),
+                    ...expenseDefault
+                }
+            });
 
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            description: '',
-            note: '',
-            createdAt: 0,
-            amount: 0,
-            id: expect.any(String)
-        }
-    });
+            return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        }).then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseDefault);
+            done();
+        });
 });
+
+// test('should setup add expense action object with default object', () => {
+//     const expenseData = {};
+
+//     const action = addExpense(expenseData);
+
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             description: '',
+//             note: '',
+//             createdAt: 0,
+//             amount: 0,
+//             id: expect.any(String)
+//         }
+//     });
+// });
