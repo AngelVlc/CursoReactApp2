9aad47bd9cdba147aa569ee5d15e81c957764889
ANGEL\Angel <angelotebh@hotmail.com>
2018-02-19 19:07:35 +0100

    Remove en firebase.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense } from '../actions/expenses';
-import { removeExpense } from '../actions/expenses';
+import { startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
@@ -11,7 +11,7 @@ export class EditExpensePage extends React.Component {
     };
 
     onRemoveClick = (e) => {
-        this.props.removeExpense({ id: this.props.expense.id });
+        this.props.startRemoveExpense({ id: this.props.expense.id });
         this.props.history.push('/');
     };
 
@@ -39,7 +39,7 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = (dispatch, props) => {
     return {
         editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-        removeExpense: (obj) => dispatch(removeExpense(obj))
+        startRemoveExpense: (obj) => dispatch(startRemoveExpense(obj))
     }
 };
 
